5c7c00522766544eaa1835bd123a16b1689f828c
emilio chiu <espinosachiuemilio@outlook.es>
2022-11-07 14:18:00 -0600

    lsp and cmp refactor

---
+++ plugin/autocomplete.lua
@@ -0,0 +1,54 @@
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+local cmp = require'cmp'
+
+local has_words_before = function()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+cmp.setup({
+    snippet = {
+	-- REQUIRED - you must specify a snippet engine
+	expand = function(args)
+	    vim.fn["vsnip#anonymous"](args.body)
+	end,
+    },
+    mapping = cmp.mapping.preset.insert({
+	['<C-b>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<C-e>'] = cmp.mapping.abort(),
+
+	-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+	["<Tab>"] = cmp.mapping(function(fallback)
+	    if cmp.visible() then
+		cmp.select_next_item()
+	    elseif vim.fn["vsnip#available"](1) == 1 then
+		feedkey("<Plug>(vsnip-expand-or-jump)", "")
+	    elseif has_words_before() then
+		cmp.complete()
+	    else
+		fallback()
+	    end
+	end, { "i", "s" }),
+	["<S-Tab>"] = cmp.mapping(function()
+	    if cmp.visible() then
+		cmp.select_prev_item()
+	    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+		feedkey("<Plug>(vsnip-jump-prev)", "")
+	    end
+	end, { "i", "s" })
+    }),
+    sources = cmp.config.sources({
+	{ name = 'nvim_lsp' },
+	{ name = 'vsnip' },
+    }, {
+	{ name = 'buffer' },
+    })
+})
--- plugin/lsp_config.lua
+++ plugin/lsp_config.lua
@@ -3,12 +3,9 @@ require("mason-lspconfig").setup()
 local set_keymap = vim.api.nvim_buf_set_keymap
 
 -- Servers
-local servers = {
-    'pyright', 'tsserver', 'jdtls', 'sumneko_lua', 'html',
-    'jsonls', 'cssls'
-}
+local servers = {'pyright', 'tsserver', 'jdtls', 'html', 'jsonls', 'cssls'}
 
--- Auto-compleate
+-- Keybinddings
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function(client, bufnr)
@@ -28,7 +25,7 @@ local on_attach = function(client, bufnr)
     set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 end
 
--- Starting-Servers
+-- Starting-Normal-Servers
 for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup {
 	capabilities = capabilities,
@@ -49,64 +46,17 @@ require('lspconfig').elixirls.setup {
     }
 }
 
--- Auto-complete
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-local cmp = require'cmp'
-
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-cmp.setup({
-    snippet = {
-	-- REQUIRED - you must specify a snippet engine
-	expand = function(args)
-	    vim.fn["vsnip#anonymous"](args.body)
-	end,
+-- Special configuration for sumneko_lua 
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
     },
-    window = {
-	-- completion = cmp.config.window.bordered(),
-	-- documentation = cmp.config.window.bordered(),
-    },
-
-    mapping = cmp.mapping.preset.insert({
-	['<C-b>'] = cmp.mapping.scroll_docs(-4),
-	['<C-f>'] = cmp.mapping.scroll_docs(4),
-	['<C-Space>'] = cmp.mapping.complete(),
-	['<C-e>'] = cmp.mapping.abort(),
-	['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-	["<Tab>"] = cmp.mapping(function(fallback)
-	    if cmp.visible() then
-		cmp.select_next_item()
-	    elseif vim.fn["vsnip#available"](1) == 1 then
-		feedkey("<Plug>(vsnip-expand-or-jump)", "")
-	    elseif has_words_before() then
-		cmp.complete()
-	    else
-		fallback()
-	    end
-	end, { "i", "s" }),
-	["<S-Tab>"] = cmp.mapping(function()
-	    if cmp.visible() then
-		cmp.select_prev_item()
-	    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-		feedkey("<Plug>(vsnip-jump-prev)", "")
-	    end
-	end, { "i", "s" })
-    }),
-    sources = cmp.config.sources({
-	{ name = 'nvim_lsp' },
-	{ name = 'vsnip' },
-    }, {
-	{ name = 'buffer' },
-    })
-})
+  },
+}
 
 -- this is for diagnositcs signs on the line number column
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
